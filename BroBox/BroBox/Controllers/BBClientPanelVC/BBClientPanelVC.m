0712af619f08d7f8f1ec679c4764a6ef73cd5378
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-02-28 12:23:26 +0100

    draw path on map

--- BroBox/BroBox/Controllers/BBClientPanelVC/BBClientPanelVC.m
+++ BroBox/BroBox/Controllers/BBClientPanelVC/BBClientPanelVC.m
@@ -8,15 +8,20 @@
 
 #import "BBClientPanelVC.h"
 
+// Frameworks
+#import <MapKit/MapKit.h>
+
 // Controllers
 #import "BBQRReaderVC.h"
 #import "BBMissionOverviewVC.h"
 
 // Managers
 #import "AppDelegate.h"
+#import "BBCanalTpManager.h"
 
 // Views
 #import "BBUserProfileCell.h"
+#import "BBMissionAnnotationView.h"
 
 typedef NS_ENUM(NSInteger, BBClientPanelSection) {
     BBClientPanelSectionInformations,
@@ -36,9 +41,12 @@ typedef NS_ENUM(NSInteger, BBClientPanelInformationRow) {
     BBClientPanelInformationRowMission,
 };
 
-@interface BBClientPanelVC () <UITableViewDataSource, UITableViewDelegate, UIAlertViewDelegate>
+@interface BBClientPanelVC () <UITableViewDataSource, UITableViewDelegate, UIAlertViewDelegate, MKMapViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
 
 @end
 
@@ -50,6 +58,7 @@ typedef NS_ENUM(NSInteger, BBClientPanelInformationRow) {
     [super viewDidLoad];
     
     [self initializeTableView];
+    [self initializeMapView];
 }
 
 #pragma mark - TableView
@@ -63,7 +72,13 @@ typedef NS_ENUM(NSInteger, BBClientPanelInformationRow) {
     self.tableView.estimatedRowHeight = 44.0;
     
     [BBUserProfileCell registerToTableView:self.tableView];
+}
+
+- (void)initializeMapView {
     
+    self.mapView.delegate = self;
+    
+    [self showMissionDetails:self.mission];
 }
 
 #pragma mark UITableviewDataSource
@@ -217,6 +232,14 @@ didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.section == BBClientPanelSectionInformations && indexPath.row == BBClientPanelInformationRowCarrier) {
+        return [BBUserProfileCell preferedHeight];
+    } else {
+        return 44.0;
+    }
+}
+
 #pragma mark - Handlers
 
 - (void)pickUpCheckinHandler {
@@ -267,12 +290,81 @@ didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     }
 }
 
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.section == BBClientPanelSectionInformations && indexPath.row == BBClientPanelInformationRowCarrier) {
-        return [BBUserProfileCell preferedHeight];
-    } else {
-        return 44.0;
+#pragma mark - MapView -
+
+
+- (void)fetchJourneyForMission:(BBParseMission *)mission {
+    
+    if (mission) {
+        BBGeoPoint *geoFrom = mission.from;
+        CLLocationCoordinate2D from = CLLocationCoordinate2DMake(geoFrom.coordinate.latitude, geoFrom.coordinate.longitude);
+        BBGeoPoint *geoTo = mission.to;
+        CLLocationCoordinate2D to = CLLocationCoordinate2DMake(geoTo.coordinate.latitude, geoTo.coordinate.longitude);
+        [BBCanalTpManager getJourneyFrom:from to:to withBlock:^(NSDictionary *json, NSError *error) {
+            if (!error) {
+                NSDictionary *journey = [[json objectForKey:@"journeys"] firstObject];
+                NSArray *path = [BBCanalTpManager pathForJourney:journey];
+                [self drawRoute:path];
+            }
+        }];
     }
+}
+
+- (void)showMissionDetails:(BBParseMission *)mission {
+    [self fetchJourneyForMission:mission];
+    
+    //    Add drop off annotation and move camera
+    BBMissionAnnotation *pickUpAnnotation = [BBMissionAnnotation annotationForMission:mission
+                                                                             withType:BBMissionAnnotationTypeFrom];
+    BBMissionAnnotation *dropOffAnnotation = [BBMissionAnnotation annotationForMission:mission
+                                                                              withType:BBMissionAnnotationTypeTo];
+    [self.mapView addAnnotation:dropOffAnnotation];
+    
+    [self.mapView showAnnotations:@[pickUpAnnotation, dropOffAnnotation] animated:YES];
+}
+
+#pragma mark AnnotationViews
+
+#define ANNOTATIONVIEW_IDENTIFIER @"missionAnnotationView"
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView
+            viewForAnnotation:(id<MKAnnotation>)annotation {
+    BBMissionAnnotationView *annotationView = (BBMissionAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:ANNOTATIONVIEW_IDENTIFIER];
+    if (!annotationView) {
+        annotationView = [[BBMissionAnnotationView alloc] initWithAnnotation:annotation
+                                                             reuseIdentifier:ANNOTATIONVIEW_IDENTIFIER];
+    }
+    annotationView.annotation = annotation;
+    return annotationView;
+}
+
+
+#pragma mark Overlay
+
+- (void)drawRoute:(NSArray *)path
+{
+    NSInteger numberOfSteps = path.count;
+    
+    CLLocationCoordinate2D coordinates[numberOfSteps];
+    for (NSInteger index = 0; index < numberOfSteps; index++) {
+        CLLocation *location = [path objectAtIndex:index];
+        CLLocationCoordinate2D coordinate = location.coordinate;
+        coordinates[index] = coordinate;
+    }
+    
+    MKPolyline *polyLine = [MKPolyline polylineWithCoordinates:coordinates count:numberOfSteps];
+    [self.mapView addOverlay:polyLine];
+}
+
+#pragma mark Overlay views
+
+- (MKOverlayView *)mapView:(MKMapView *)mapView viewForOverlay:(id <MKOverlay>)overlay {
+    MKPolylineView *polylineView = [[MKPolylineView alloc] initWithPolyline:overlay];
+    polylineView.strokeColor = [UIColor blueColor];
+    polylineView.lineWidth = 5.0;
+    polylineView.alpha = 0.7;
+    
+    return polylineView;
 }
 
 @end
