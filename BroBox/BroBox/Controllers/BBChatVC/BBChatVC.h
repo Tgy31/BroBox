33f1a24f64267f23171258501497114511be86bc
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-03-01 17:48:08 +0100

    BBChatVC

---
+++ BroBox/BroBox/Controllers/BBChatVC/BBChatVC.h
@@ -0,0 +1,13 @@
+//
+//  BBChatVC.h
+//  BroBox
+//
+//  Created by Tanguy Hélesbeux on 01/03/2015.
+//  Copyright (c) 2015 Brobox. All rights reserved.
+//
+
+#import "SLKTextViewController.h"
+
+@interface BBChatVC : SLKTextViewController
+
+@end
